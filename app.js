d4c35e3681d01850f0038a027abd9e9c9fb22432
amberrr0420 <amberrr0420@gmail.com>
2022-01-07 12:42:47 +0800

    show 80 movies & searching movies by title

---
+++ app.js
@@ -0,0 +1,36 @@
+//使用 require 載入 Express
+const express = require('express')
+const app = express()
+const port = 3000
+
+//使用 require 載入 Express-handlebars
+const exphbs = require('express-handlebars')
+const movieList = require('./movies.json')
+
+//setting template-engine
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+//告訴 Express 靜態檔案的資料夾位置
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index', { movies: movieList.results })
+})
+
+app.get('/movies/:movie_id', (req, res) => {
+  const movie = movieList.results.find(movie => movie.id == req.params.movie_id)
+  res.render('show', { movie: movie })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const movies = movieList.results.filter(movie => {
+    return movie.title.toLocaleLowerCase().includes(keyword)
+  })
+  res.render('index', { movies: movies, keyword: keyword })
+})
+
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
