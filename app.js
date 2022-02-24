1177ba3b01a46b2d2b24e3eb83924b1ae21ee6a0
amberrr0420 <amberrr0420@gmail.com>
2022-02-24 15:14:47 +0800

    change port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //使用 require 載入 Express
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 
 //使用 require 載入 Express-handlebars
 const exphbs = require('express-handlebars')
